cab59e5f1f75831f5d8dcba80614eeffaf415057
Alimahdi Inanloo <aliinanlou20@gmail.com>
2025-10-17 00:21:11 +0300

    Add files via upload

---
+++ temperatur to farenheit/temperatur to farenheit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CelsiusToFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius, fahrenheit;
+
+            Console.Write("Enter a temperature (Celsius): ");
+
+            celsius = Convert.ToDouble(Console.ReadLine());
+            // celsius = double.Parse(Console.ReadLine());
+
+            fahrenheit = (celsius * 9 / 5) + 32;
+
+            Console.WriteLine("{0} °C is {1} °F", celsius, fahrenheit);
+        }
+    }
+}
