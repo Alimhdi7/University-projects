df3bba9a09c28108dc2c4213553267348a0f17b6
Alimahdi Inanloo <aliinanlou20@gmail.com>
2025-10-17 00:10:21 +0300

    Add files via upload

---
+++ mohit masahat dayere/mohit masahat dayere/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Mohit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, mohit, masahat;
+            const double PI = 3.141592653589793;
+
+            Console.Write("Enter radius: ");
+            a = double.Parse(Console.ReadLine());
+
+            mohit = 2 * PI * a;        // محیط = 2πr
+            masahat = PI * a * a;      // مساحت = πr²
+
+            Console.WriteLine("Masahat is {0} and Mohit is {1}", masahat, mohit);
+
+            Console.ReadKey();
+        }
+    }
+}
