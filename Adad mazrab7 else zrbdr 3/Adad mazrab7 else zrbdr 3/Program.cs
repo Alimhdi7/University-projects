9310d62ffcd423107b97a4de4e5e4ffda354db09
Alimahdi Inanloo <aliinanlou20@gmail.com>
2025-10-24 17:02:33 +0300

    Add files via upload

---
+++ Adad mazrab7 else zrbdr 3/Adad mazrab7 else zrbdr 3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter number:");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int number))
+        {
+            if (number % 7 == 0)
+            {
+                Console.WriteLine("YES!");
+            }
+            else
+            {
+                int result = number * 3;
+                Console.WriteLine(result);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid entry,please enter integer number");
+        }
+    }
+}
