da0319825f82b566e753ce034da939acb0046ba1
Alimahdi Inanloo <aliinanlou20@gmail.com>
2025-10-24 16:59:18 +0300

    Add files via upload

---
+++ adade kochk tr az 10 va zoj/adade kochk tr az 10 va zoj/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter number:");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int number))
+        {
+            if (number < 10 && number % 2 == 0)
+            {
+                Console.WriteLine("True!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid entry,please enter integer number");
+        }
+    }
+}
