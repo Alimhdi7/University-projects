16f5fdbf41f9999de1620f79d3670ee5325b31e1
Alimahdi Inanloo <aliinanlou20@gmail.com>
2025-11-08 01:59:57 +0300

    Add files via upload

---
+++ hasele zrbe adade vorodi/hasele zrbe adade vorodi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        long product = 1;
+
+        while (true)
+        {
+            Console.Write("Enter a number,(to end enter 0) ");
+
+            try
+            {
+                int number = Int32.Parse(Console.ReadLine());
+
+                if (number == 0)
+                {
+                    break;
+                }
+
+                product *= number;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error The input is invalid. Please enter a number:");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Error The entered number is outside the allowed range");
+            }
+        }
+
+        Console.WriteLine($"\nThe final product of the entered numbers:{product}");
+    }
+}
